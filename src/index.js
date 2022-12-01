808c22eab0cb2f2aa81232547085636c3b7a39d7
rosskovt <rostikk@gmail.com>
2022-12-01 15:56:30 +0200

    multi-face detection included.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import 'tachyons';
 
 const root = createRoot(document.getElementById('root'));
